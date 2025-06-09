2c97af1695127299b90a2a3e0e72188f55d49f7c
kilingfred <mr.perepolkin@gmail.com>
2025-06-09 14:39:26 +0300

    added express urlencoded

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const pool = new Pool({
 
 app.set("views", path.join(__dirname, "/public/html"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
